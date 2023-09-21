807d0dd1acf6d060f5976676ee45cad9e7e3bc74
aswatson <aswatson95@gmail.com>
2023-09-20 20:04:29 -0600

    added to

--- wordCount.py
+++ wordCount.py
@@ -1,10 +1,11 @@
 import os
 import re
 import sys
+import string
 
 #Step 0: Check path for files and correct command line calls
 # set input and output files
-if len(sys.argv) is not 3:
+if len(sys.argv) != 3:
     print("Correct usage: wordCount.py <input text file> <output file>")
     exit()
 
@@ -28,5 +29,25 @@ else:
 wordDiction = {}
 
 #Step 1: Read the file absorbing words into the dictionary
+with open(inputFile, 'r') as inputFile:
+    text = inputFile.read()
+
+    translator = str.maketrans('','',string.punctuation)
+    text = text.translate(translator).lower()
+
+    words = text.split()
+
+    for word in words:
+        if word in wordDiction:
+            wordDiction[word] += 1
+        else:
+            wordDiction[word] = 1
+
+sort = sorted(wordDiction.items(), key=lambda x: x[0])
+
 
 #Step 2: Output the dictionary to a file overriding the file if it exists
+with open(outputFile, 'w') as output:
+    for word in wordDiction:
+        count = wordDiction[word]
+        output.write(f"{word} {count}\n")
